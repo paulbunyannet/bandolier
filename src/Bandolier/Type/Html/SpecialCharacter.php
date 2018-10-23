2d43e94b72b5bd40a6396176bd2baf4c14fb0ca9
Nate Nolting <me@natenolting.com>
2018-10-23 13:23:05 -0500

    Added superscript HTML helper
    Wrap special characters that should be super scripted with the <sup> tag.

---
+++ src/Bandolier/Type/Html/SpecialCharacter.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Pbc\Bandolier\Type\Html;
+
+use Pbc\Bandolier\Type\BaseType;
+
+class SpecialCharacter extends BaseType
+{
+    /**
+     * Superscript special characters
+     * @see https://en.wikipedia.org/wiki/Registered_trademark_symbol#Related_symbols
+     * @param $string string to modify
+     * @param array $characters List of characters to find and apply superscript
+     * @return string
+     */
+    public static function superscriptSpecialCharacters($string, array $characters = []) {
+
+        $defaults = [
+            'registered' => ['®', '&reg;', '&#x00AE;', '&#174;'],
+            'trademark'  => ['™', '&trade;', '&#8482;'],
+            'copyright'  => ['©', '&#169;', '&copy;'],
+            'service'    => ['℠', '&#8480;'],
+            'phonogram'  => ['℗', '&#8471;'],
+            'hechsher'   => ['Ⓤ', '&#9418;']
+        ];
+        $characters = array_merge($defaults, $characters);
+        $specials = array();
+        array_walk_recursive($characters, function ($v) use (&$specials) {
+            $specials[] = $v;
+        });
+        // wrap all the special characters above with superscript tags
+        for ($i = 0, $iCount = count($specials); $i < $iCount; $i++) {
+            if (strpos($string, $specials[0]) !== false) {
+                $string = str_replace($specials[$i], '<sup>' . $specials[$i] . '</sup>', $string);
+            }
+        }
+
+        return $string;
+    }
+}
